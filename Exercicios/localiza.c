987cd79965dd2eb149cd89a253f7a536cb9b40b2
GABRIEL MENEGON CASSANO <canalbiblioguia@gmail.com>
2026-05-13 16:06:18 -0300

    Update do arquivo localiza

--- Exercicios/localiza.c
+++ Exercicios/localiza.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 int main()
 {
@@ -8,28 +9,61 @@ int main()
     int km = 0;
     int dias = 0;
     char nome[50];
+    char carro[50];
     float total_km = 0.0;
     float total_dias = 0.0;
     const float VALOR_KM = 1.25;
     const float VALOR_DIA = 99.50;
+    const char cupons[3][30] = {
+        "GMC_10", "GMC_20", "GMC_30"
+    };
+    char cupomCliente[30];
+    int temCupom = 0;
+    float valorDesconto = 0;
 
     printf("Digite o nome do cliente: ");
     scanf("%s", nome);
 
     printf("Obrigado por nos escolher, %s", nome);
 
-    printf("\nQuantos Km's você irá rodar? ");
+    printf("\nQual o nome do carro? ");
+    scanf("%s", carro);
+
+    printf("Quantos Km's você irá rodar? ");
     scanf("%i", &km);
 
     printf("Por quantos dias o carro será usado? ");
     scanf("%i", &dias);
 
-    total_km = km * VALOR_KM;   
-    total_dias = dias * VALOR_DIA;
+    printf("Tem cupom de desconto? (1 - Sim; 0 - Não) ");
+    scanf("%i", &temCupom);
 
-    printf("%s, segue um relatório detalhado", nome);
+    if (temCupom == 1) {
+        printf("Digite o código: ");
+        scanf("%s", cupomCliente);
+
+        for (int i = 0; i < 3; i++) {
+            if (strcmp(cupomCliente, "GMC_10") == 0) {
+                valorDesconto = 10;
+            } else if (strcmp(cupomCliente, "GMC_20") == 0) {
+                valorDesconto = 20;
+            } else if (strcmp(cupomCliente, "GMC_30") == 0) {
+                valorDesconto = 30;
+            }
+        }
+    }
+
+    total_km = (km * VALOR_KM) - valorDesconto;   
+    total_dias = (dias * VALOR_DIA) - valorDesconto;
+
+    printf("\n%s, segue um relatório detalhado:", nome);
+    printf("\nCarro: %s", carro);
     printf("\nTotal por Km: R$ %.2f", total_km);
     printf("\nTotal por Dia: R$ %.2f", total_dias);
+
+    if (temCupom == 1) {
+        printf("\nVocê teve desconto de R$ %.2f", valorDesconto);
+    }
 
     if (total_dias < total_km) {
         printf("\nSugerimos o plano por dia!");
@@ -38,6 +72,7 @@ int main()
     }
 
     printf("\nTenha um ótima tarde!");
+    printf("\n--- Este programa foi desenvolvido por Gabriel Menegon Cassano ---");
 
     return 0;
 }
